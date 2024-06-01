87b3d8cc6afdd8b29436614ee929ec38a0c8e87b
NathanSnail <47441022+NathanSnail@users.noreply.github.com>
2024-06-02 03:53:55 +1200

    ????

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 import struct
 import sys
 import xml.dom.minidom
+from typing import Any
 from xml.dom.minidom import parseString
 
 fastlz = ctypes.cdll.LoadLibrary("./fastlz.so")
@@ -41,7 +42,6 @@ class Reader:
 			self.ptr += 1
 			if x == 0x00:
 				return val
-			print(x)
 			val += bytes([x])
 
 	def skip(self, count: int):
@@ -57,16 +57,19 @@ class ComponentFieldData:
 	size: int
 	typename: str
 	field: str
-	variable_size: bool
+	variable_size: list[int]
 
 
 class Component:
 	name: str
+	tags: list[str]
+	fields: dict[str, Any]
 
 
 class Entity:
 	name: str
 	path: str
+	tags: list[str]
 	x: float
 	y: float
 	size_x: float
@@ -81,7 +84,6 @@ compressed_reader = Reader(compressed_data)
 compressed_size, decompressed_size = compressed_reader.read_le(
 	4
 ), compressed_reader.read_le(4)
-print(compressed_size, decompressed_size)
 input_buffer = ctypes.create_string_buffer(
 	compressed_data[compressed_reader.ptr :], compressed_size
 )
@@ -95,7 +97,6 @@ open("./out", "wb").write(decompressed)
 data_reader = Reader(decompressed)
 data_reader.skip(8)
 hash = data_reader.read_bytes(0x20)
-print(hash)
 schema_content = open(
 	"/home/nathan/Documents/code/noitadata/data/schemas/" + str(hash)[2:-1] + ".xml",
 	"r",
@@ -128,11 +129,15 @@ for i in tree.documentElement.childNodes:
 		var_name = child.getAttribute("name")
 		var_size = child.getAttribute("size")
 		var_type = child.getAttribute("type")
-		var_variable_size = False
+		var_variable_size = []
 		if "<" in var_type:
 			var_special_t = var_type.split(" ")[1].split("<")[0]
 			if var_special_t == "std::basic_string" or var_special_t == "std::vector":
-				var_variable_size = True
+				var_variable_size = [0]
+				var_size = "0"
+			if var_special_t == "class ConfigExplosion":
+				print("??")
+				var_variable_size = [5]
 
 		data = ComponentFieldData()
 		data.typename = var_type
@@ -144,39 +149,47 @@ for i in tree.documentElement.childNodes:
 			type_sizes[var_type] = data.size
 
 maybe_num_entities = data_reader.read_be(4)
-print(maybe_num_entities)
 
 
 def parse_entity(reader: Reader):
 	name_len = reader.read_be(4)
-	name = reader.read_bytes(name_len)
+	name = bstr(reader.read_bytes(name_len))
+	print("N", name)
 	reader.skip(1)  # 0x00
-	print(name)
 	path_len = reader.read_be(4)
-	path = reader.read_bytes(path_len)
-	print(path)
+	path = bstr(reader.read_bytes(path_len))
+	print("!", path)
 	tag_len = reader.read_be(4)
-	tag = reader.read_bytes(tag_len)
-	print(tag)
+	tag = bstr(reader.read_bytes(tag_len)).split(",")
 	x = reader.read_float()
 	y = reader.read_float()
 	scale_x = reader.read_float()
 	scale_y = reader.read_float()
 	rotation = reader.read_float()
-	print(x, y, scale_x, scale_y, rotation)
 	maybe_num_comps = reader.read_be(4)
-	print(maybe_num_comps)
-	parse_component(reader)
+	entity = Entity()
+	entity.name = name
+	entity.path = path
+	entity.tags = tag
+	entity.x = x
+	entity.y = y
+	entity.size_y = scale_x
+	entity.size_y = scale_y
+	entity.rotation = rotation
+	entity.components = []
+	for _ in range(maybe_num_comps):
+		entity.components.append(parse_component(reader))
+	return entity
 
 
 def bstr(a):
 	return str(a)[2:-1]
 
 
-def do_type(data, t):
+def do_type(data, t, f):
 	vec2 = "class ceng::math::CVector2<"
 	if t == "bool":
-		data = data == b"\x00"
+		data = data == b"\x01"
 	elif t == "float":
 		data = struct.unpack("f", data[::-1])[0]
 	elif t == "int":
@@ -185,29 +198,46 @@ def do_type(data, t):
 		true_type = t[len(vec2) : -1]
 		true_size = type_sizes[true_type]
 		data = (
-			do_type(data[:true_size], true_type),
-			do_type(data[true_size:], true_type),
+			do_type(data[:true_size], true_type, f),
+			do_type(data[true_size:], true_type, f),
 		)
 	return data
 
 
-def parse_component(reader: Reader):
+def parse_component(reader: Reader) -> Component:
+	comp = Component()
 	component_name_len = reader.read_be(4)
 	component_name = bstr(reader.read_bytes(component_name_len))
-	print(component_name)
 	reader.skip(2)  # 0x0101
 	component_tag_len = reader.read_be(4)
-	component_tags = reader.read_bytes(component_tag_len)
-	print(component_tags)
+	component_tags = bstr(reader.read_bytes(component_tag_len))
 	fields = component_data[component_name]
 	data = {}
 	for field in fields:
 		look_size = field.size
-		if field.variable_size:
-			look_size = reader.read_be(4)
-		val = reader.read_bytes(look_size)
-		data[field.field] = do_type(val, field.typename)
-	print(data)
+		print(field.field, look_size, field.typename, hex(reader.ptr))
+		start = reader.ptr
+		eaten = 0
+		val = b""
+		for offset in field.variable_size:
+			print(offset)
+			val += reader.read_bytes((offset + start + eaten) - reader.ptr)
+			to_skip = reader.read_be(4)
+			eaten += to_skip
+			scale = reader.read_bytes(to_skip)
+			print(scale)
+			val += scale
+		if len(field.variable_size) == 0:
+			val = reader.read_bytes(look_size)
+		data[field.field] = do_type(val, field.typename, field.field)
+		print(field.field, data[field.field])
+	comp.fields = data
+	comp.name = component_name
+	comp.tags = component_tags.split(",")
+	return comp
 
 
-parse_entity(data_reader)
+for _ in range(maybe_num_entities):
+	parse_entity(data_reader)
+	data_reader.skip(4)  # ???
+	print(data_reader.data[data_reader.ptr :])
