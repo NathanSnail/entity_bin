bc93d779886e442d33614eb626803b5cb086a318
NathanSnail <47441022+NathanSnail@users.noreply.github.com>
2024-06-13 18:24:11 +1200

    yay

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-windows = False  # change False to True for windowsj
+import os
+windows = os.name == "nt"
 if windows:
 	schema_path = "C:/Program Files (x86)/Steam/steamapps/common/Noita/data/schemas/"
 else:
-	schema_path = "/home/nathan/Documents/code/noitadata/data/schemas/"
+	schema_path = "~/.local/share/Steam/steamapps/common/Noita/data/schemas/"
