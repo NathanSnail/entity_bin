c1691d3732fcd2f4077c93e5c533a4c6420ff56e
Nathan <nathan.titirangi@gmail.com>
2024-06-22 00:20:28 +1200

    linux home fail??

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ windows = os.name == "nt"
 if windows:
 	schema_path = "C:/Program Files (x86)/Steam/steamapps/common/Noita/data/schemas/"
 else:
-	schema_path = "~/.local/share/Steam/steamapps/common/Noita/data/schemas/"
+	schema_path = os.path.expanduser("~/.local/share/Steam/steamapps/common/Noita/data/schemas/")
